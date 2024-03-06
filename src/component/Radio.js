032bcdee05baf187c33619e0fad82f5bc020d836
SYL4Git <seungyeollee4@gmail.com>
2024-03-06 12:05:35 +0900

    240306 1205 @class

--- src/component/Radio.js
+++ src/component/Radio.js
@@ -1,28 +1,24 @@
-import { useContext } from "react";
-import RadioContext from "./RadioContext";
+import { useContext } from 'react';
+import RadioContext from './RadioContext';
 
-const Radio = ({
-    className,
-    name,
-    value,
-    disabled,
-    children,
-}) => {
-    const group = useContext(RadioContext);
+const Radio = ({ className, name, value, disabled, children }) => {
+	const group = useContext(RadioContext);
 
-    return (
-        <label>
-            <input
-                className={`radio ${className}`}
-                type="radio"
-                name={name}
-                value={value}
-                disabled={disabled || group.disabled}
-                checked={group.value !== undefined ? value === group.value : undefined}
-                onChange={(e) => group.onChange && group.onChange(e.target.value)}
-            />
-            {children}
-        </label>
-    );
+	return (
+		<label>
+			<input
+				className={`radio ${className}`}
+				type='radio'
+				name={name}
+				value={value}
+				disabled={disabled || group.disabled}
+				checked={
+					group.value !== undefined ? value === group.value : undefined
+				}
+				onClick={(e) => group.onChange && group.onChange(e.target.value)}
+			/>
+			{children}
+		</label>
+	);
 };
 export default Radio;
